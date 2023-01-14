5faf889c2fb2ecc0b7dca9820534aa71edf970ae
Gabriel Paulo <gabriellemos11@hotmail.com>
2023-01-14 05:42:24 +0000

    configuring dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express'
+import * as dotenv from 'dotenv'
+
 const app = express()
+
+dotenv.config()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
