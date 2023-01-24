361f17f5d8d307cef849fb5f3a6cba4d787537a0
Gabriel Paulo <gabriellemos11@hotmail.com>
2023-01-24 08:45:02 +0000

    fixing error signup validation

--- src/validation/signup.ts
+++ src/validation/signup.ts
@@ -5,17 +5,20 @@ const usernameRegex = /^([a-zA-Z0-9.]+_+){0,1}([a-zA-Z0-9])+$/g
 const schema = Yup.object().shape({
   username: Yup.string()
     .matches(usernameRegex, 'nome de usuario invalido')
-    .typeError('nome de usuario invalido'),
+    .typeError('nome de usuario invalido')
+    .required('nome de usuario obrigatorio'),
 
   email: Yup.string()
-    .required('email é obrigatorio')
     .email('email invalido')
-    .typeError('email invalido'),
+    .typeError('email invalido')
+    .required('email é obrigatorio'),
     
   password: Yup.string()
     .min(8, 'minimo 8 digitos')
     .max(20, 'maximo 20 digitos')
     .typeError('senha invalida')
+    .required('senha obrigatorio'),
+
 })
 
 export default schema
