1e10c4ce7f75503c994af0f0998336b61b52f210
Gabriel Paulo <gabriellemos11@hotmail.com>
2023-01-24 04:48:45 +0000

    creating user model

---
+++ src/models/User.ts
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose'
+
+interface IUser {
+  account: {
+    id: mongoose.Types.ObjectId,
+    username: String
+  }
+  follower: [],
+  followers: [],
+  avatar: string,
+  isPrivate: false,
+  createdAt: Date,
+  updatedAt: Date,
+}
+
+const UserSchema = new mongoose.Schema<IUser>({
+  account: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    username: {
+      type: String,
+      require: true
+    }
+  },
+  follower: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  avatar: {
+    type: String,
+    default: '1675d7e4-cd73-4d78-a522-e4f71cba88e5.png'
+  },
+  isPrivate: {
+    type: Boolean,
+    default: false
+  },
+}, {
+  timestamps: true
+})
+
+const User = mongoose.model<IUser>('User', UserSchema)
+
+export { IUser }
+export default User
