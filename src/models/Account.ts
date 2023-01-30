4aaa08e5ef9afe680b0217635863ac347d23295f
Gabriel Paulo <gabriellemos11@hotmail.com>
2023-01-30 06:13:07 +0000

    update Account model

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -1,23 +1,28 @@
 import mongoose from 'mongoose'
 
 interface IAccount {
-  username: string,
-  email: string,
-  password: string,
-  createdAt: Date,
-  updatedAt: Date,
+  username: string
+  email: string
+  password: string
+  createdAt: Date
+  updatedAt: Date
 }
 
 const AccountSchema = new mongoose.Schema<IAccount>({
   username: {
     type: String,
     unique: true,
+    required: true
   },
   email: {
     type: String,
-    unique: true
+    unique: true,
+    required: true
   },
-  password: { type: String }
+  password: {
+    type: String,
+    required: true
+  }
 }, {
   timestamps: true
 })
